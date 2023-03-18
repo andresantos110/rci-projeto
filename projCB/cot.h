86c1089d8864fd5d767202ea75c9b82753f82ea9
André Santos <106824104+andresantos110@users.noreply.github.com>
2023-03-18 16:10:18 +0000

    cb

---
+++ projCB/cot.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+struct node{
+    char id;
+    char ext;
+    char bck;
+    int intr[128+1];
+};
